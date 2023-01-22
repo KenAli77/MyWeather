6d9866823c4c8a6e5cbbad12acdb8e9fd69ac4ed
Ken <kenediidali@gmail.com>
2023-01-22 11:40:30 +0100

    setup

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -3,20 +3,16 @@ import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Weather } from '../WeatherData';
+import { environment } from 'src/environments/environment';
 
 
-const htttpOptions = {
-  headers: new HttpHeaders({
-    'Content-type':'application/json'
-  })
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-apiKey = "27902584cd441d3bccd41c4477598842"
+apiKey = environment.apiKey
 units = "metric"
 
 apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="
